1c8bb84119325f4e93109be88f4586684a7cfd0b
pikachu987 <pikachu987@naver.com>
2016-08-06 18:00:58 +0900

    icon commit

--- ClubfulIOS/AppDelegate.swift
+++ ClubfulIOS/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -26,7 +28,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationDidBecomeActive(application: UIApplication) {
         //생명주기 앱살아났을때
+        
+        //지금 최신 위치 이동
         vcMyLocationMove = true
+        
+        
+        //앱 통신
+        var user = Storage.getRealmUser()
+        let parameters : [String: AnyObject] = ["appType": "ios", "appVersion": Util.nsVersion, "deviceCD": NSDate().getFullDate(), "language": Util.language, "deviceId": Util.deviceId, "token": user.token, "categoryVer": user.categoryVer, "noticeVer": user.noticeVer]
+        Alamofire.request(.GET, URL.version_Check, parameters: parameters)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseData { response in
+                let data : NSData = response.data!
+                let dic = Util.convertStringToDictionary(data)
+                if let code = dic["code"] as? Int{
+                    if code == 0{
+                        user = Storage.copyUser()
+                        user.token = dic["token"] as! String
+                        Util.newVersion = dic["iosVersion"] as! String
+                        user.categoryVer = dic["categoryVer"] as! Int
+                        user.noticeVer = dic["noticeVer"] as! Int
+                        if let categoryList = dic["categoryList"] as? [[String: AnyObject]]{
+                            Storage.setStorage("categoryList", value: categoryList)
+                        }
+                        Storage.setRealmUser(user)
+                    }
+                }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
 
